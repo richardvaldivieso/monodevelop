ae756063cf3ae249868fb4fd2a5500ca7083af42
richardvaldivieso <richardvaldiviesom@gmail.com>
2014-07-03 23:44:28 -0500

    This is part of the challenge.
    I am creating this template for my test.

---
+++ main/tests/UserInterfaceTests/ChallengeRV.cs
@@ -0,0 +1,96 @@
+﻿//
+// ChallengeRV.cs
+//
+// Author:
+//       richardvaldivieso <${AuthorEmail}>
+//
+// Copyright (c) 2014 richardvaldivieso
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using MonoDevelop.Core;
+using MonoDevelop.Ide.Commands;
+using NUnit.Framework;
+
+namespace UserInterfaceTests
+{
+	public class ChallengeRV: UITestBase
+	{
+		public ChallengeRV ()
+		{
+		}
+
+		[Test]
+		public void OpenEditCompile ()
+		{
+			var slnFile = Ide.OpenTestSolution ("ConsoleApp-VS2010/ConsoleApplication.sln");
+			var slnDir = slnFile.ParentDirectory;
+
+			var exe = slnDir.Combine ("bin", "Debug", "XamarinChallenge.exe");
+			Assert.IsFalse (File.Exists (exe));
+
+			Ide.OpenFile (slnFile.ParentDirectory.Combine ("Program.cs"));
+
+			Ide.BuildSolution ();
+			AssertExeHasOutput (exe, "");
+
+			Session.SelectActiveWidget ();
+			for (int n = 0; n < 10; n++)
+				Session.ExecuteCommand (TextEditorCommands.LineDown);
+			Session.ExecuteCommand (TextEditorCommands.LineEnd);
+
+			//List<string> s = new List<string> () {"one", "two", "three"};
+			//Session.TypeText("\nList<string> s = new List<string>(){\"one\"};");
+			Session.TypeText("\nConsole.WriteLine(\"Hello Xamarin\");\n");
+
+			Ide.BuildSolution ();
+			AssertExeHasOutput (exe, "Hello Xamarin");
+
+			Ide.CloseAll ();
+		}
+		void AssertExeHasOutput (string exe, string expectedOutput)
+		{
+			var sw = new StringWriter ();
+			var p = ProcessUtils.StartProcess (new ProcessStartInfo (exe), sw, sw, CancellationToken.None);
+			Assert.AreEqual (0, p.Result);
+			string output = sw.ToString ();
+
+			Assert.AreEqual (expectedOutput, output.Trim ());
+		}
+		[Test]
+		public void CreateBuildProject ()
+		{
+			string projectName = "XamarinChallenge";
+			string projectCategory = "C#";
+			string projectKind = "Console Project";
+
+			var projectDirectory = Util.CreateTmpDir (projectName);
+
+			Ide.CreateProject (projectName, projectCategory, projectKind, projectDirectory);
+
+			Ide.BuildSolution ();
+
+			Ide.CloseAll ();
+		}
+	}
+}
+
---
+++ main/tests/test-projects/ConsoleApp-Empty/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
